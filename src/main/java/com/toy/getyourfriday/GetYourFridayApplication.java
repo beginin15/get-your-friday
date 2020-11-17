bf195795920ae03fcf1746d41bb1ef94ab0201e5
Jay <beginin15@gmail.com>
2020-11-18 04:35:20 +0900

    [#1] init Project

---
+++ src/main/java/com/toy/getyourfriday/GetYourFridayApplication.java
@@ -0,0 +1,13 @@
+package com.toy.getyourfriday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetYourFridayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetYourFridayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toy/getyourfriday/GetYourFridayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.getyourfriday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetYourFridayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
