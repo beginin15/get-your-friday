4d6ec93b9c0b3ff62c2ae50b13af82bdf78b34be
Jay <beginin15@gmail.com>
2020-11-25 22:26:05 +0900

    [#2] test: headless 모드 성능 테스트

--- src/test/java/com/toy/getyourfriday/selenium/WebDriverTest.java
+++ src/test/java/com/toy/getyourfriday/selenium/WebDriverTest.java
@@ -10,6 +10,7 @@ import java.io.File;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class WebDriverTest {
 
@@ -40,5 +41,23 @@ public class WebDriverTest {
     public void create() {
         driver = new RemoteWebDriver(driverService.getUrl(), new ChromeOptions());
         assertNotNull(driver);
+    }
+
+    @Test
+    @DisplayName("UI 유무에 따른 성능 차이 테스트")
+    public void headlessMode() {
+        // when - UI 있는 경우
+        long headMode = System.currentTimeMillis();
+        driver = new RemoteWebDriver(driverService.getUrl(), new ChromeOptions());
+        long resultOfheadMode = System.currentTimeMillis() - headMode;
+        driver.quit();
+
+        // when - UI 없는 경우
+        long headlessMode = System.currentTimeMillis();
+        driver = new RemoteWebDriver(driverService.getUrl(), new ChromeOptions().setHeadless(true));
+        long resultOfheadless = System.currentTimeMillis() - headlessMode;
+
+        // then
+        assertTrue(resultOfheadless < resultOfheadMode);
     }
 }
