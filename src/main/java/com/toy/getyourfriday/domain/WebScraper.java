7bba084224dacb695eb7247b395c6403978e0bd2
Jay <beginin15@gmail.com>
2020-12-28 15:37:07 +0900

    [#10] refactor: 정적 팩토리 메소드 정의

--- src/main/java/com/toy/getyourfriday/domain/WebScraper.java
+++ src/main/java/com/toy/getyourfriday/domain/WebScraper.java
@@ -3,6 +3,8 @@ package com.toy.getyourfriday.domain;
 import com.toy.getyourfriday.component.ProductContainer;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.toList;
@@ -20,6 +22,10 @@ public class WebScraper implements Runnable {
         this.driver = driver;
         this.modelUrl = url;
         this.productContainer = productContainer;
+    }
+
+    public static WebScraper of(ChromeOptions options, ModelUrl modelUrl, ProductContainer productContainer) {
+        return new WebScraper(new ChromeDriver(options), modelUrl, productContainer);
     }
 
     @Override
