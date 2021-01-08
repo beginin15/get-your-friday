23ef512ee2a4e2e938f3238875b788114c433042
Jay <beginin15@gmail.com>
2021-01-09 03:54:59 +0900

    [#12] refactor: spring-data-dynamodb Config 추가
    - 환경(로컬, 배포)에 따라 달라져야 하는 빈은 프로필로 구분

--- src/main/java/com/toy/getyourfriday/config/DynamoDBConfig.java
+++ src/main/java/com/toy/getyourfriday/config/DynamoDBConfig.java
@@ -1,11 +1,86 @@
 package com.toy.getyourfriday.config;
 
+import com.amazonaws.auth.*;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverter;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.toy.getyourfriday.domain.ModelUrl;
+import com.toy.getyourfriday.domain.UserRepository;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+import static com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
 
 @Configuration
+@EnableDynamoDBRepositories(basePackageClasses = UserRepository.class)
 public class DynamoDBConfig {
+
+    @Value("${amazon.aws.accesskey}")
+    private String amazonAWSAccessKey;
+
+    @Value("${amazon.aws.secretkey}")
+    private String amazonAWSSecretKey;
+
+    @Value("${amazon.aws.dynamodb.endpoint}")
+    private String dynamoDBEndpoint;
+
+    public AWSCredentialsProvider amazonAWSCredentialsProvider() {
+        return new AWSStaticCredentialsProvider(amazonAWSCredentials());
+    }
+
+    @Bean
+    public AWSCredentials amazonAWSCredentials() {
+        return new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
+    }
+
+    @Bean
+    @Primary
+    public DynamoDBMapperConfig dynamoDBMapperConfig() {
+        return DynamoDBMapperConfig.DEFAULT;
+    }
+
+    @Bean
+    @Primary
+    public DynamoDBMapper dynamoDBMapper(AmazonDynamoDB amazonDynamoDB, DynamoDBMapperConfig config) {
+        return new DynamoDBMapper(amazonDynamoDB, config);
+    }
+
+    @Bean
+    public EndpointConfiguration endpointConfiguration() {
+        return new EndpointConfiguration(dynamoDBEndpoint, Regions.AP_NORTHEAST_2.getName());
+    }
+
+    @Profile("dev")
+    @Bean(name = "amazonDynamoDB")
+    public AmazonDynamoDB localAmazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder.standard()
+                .withCredentials(amazonAWSCredentialsProvider())
+                .withEndpointConfiguration(endpointConfiguration())
+                .build();
+    }
+
+    @Profile("!dev")
+    @Bean
+    public AmazonDynamoDB amazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder.standard()
+                .withCredentials(InstanceProfileCredentialsProvider.getInstance())
+                .withRegion(Regions.AP_NORTHEAST_2)
+                .build();
+    }
+
+    @Profile("dev")
+    @Bean
+    public DynamoDB dynamoDB() {
+        return new DynamoDB(localAmazonDynamoDB());
+    }
 
     public static class ModelUrlConverter implements DynamoDBTypeConverter<String, ModelUrl> {
 
