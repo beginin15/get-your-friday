023230e8f401bd5e4bc8b43bd0e8a0de565b2376
Jay <beginin15@gmail.com>
2021-01-08 18:09:45 +0900

    [#12] test: DynamoDBMapper를 사용한 CRUD 테스트

---
+++ src/test/java/com/toy/getyourfriday/dynamoDB/DynamoDBMapperTest.java
@@ -0,0 +1,116 @@
+package com.toy.getyourfriday.dynamoDB;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.model.DeleteTableRequest;
+import com.amazonaws.services.dynamodbv2.util.TableUtils;
+import com.toy.getyourfriday.component.ModelUrlParser;
+import com.toy.getyourfriday.config.DynamoDBConfig;
+import com.toy.getyourfriday.domain.User;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import static com.toy.getyourfriday.dynamoDB.AwsDynamoDBSdkTest.TABLE_NAME;
+import static com.toy.getyourfriday.dynamoDB.AwsDynamoDBSdkTest.createUserTableRequest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest(classes = {
+        ModelUrlParser.class,
+        DynamoDBConfig.class
+})
+@Import(DynamoDBTestConfig.class)
+public class DynamoDBMapperTest {
+
+    @Autowired
+    private AmazonDynamoDB amazonDynamoDB;
+
+    @Autowired
+    private DynamoDB dynamoDB;
+
+    @Autowired
+    private DynamoDBMapper dynamoDBMapper;
+
+    @Autowired
+    private ModelUrlParser modelUrlParser;
+
+
+    @BeforeAll
+    void createTable() {
+        TableUtils.deleteTableIfExists(amazonDynamoDB, new DeleteTableRequest().withTableName(TABLE_NAME));
+        dynamoDB.createTable(createUserTableRequest(
+                TABLE_NAME,
+                "chatId",
+                "monitoredUrl")
+        );
+    }
+
+    @Test
+    @DisplayName("User 추가")
+    void putItem() {
+        // given
+        User user = new User(1234, modelUrlParser.findByName("lassie"));
+
+        // when
+        dynamoDBMapper.save(user);
+
+        assertThat(dynamoDBMapper.load(User.class, 1234)).isNotNull();
+    }
+
+    @Test
+    @DisplayName("User 조회")
+    void getItem() {
+        // given
+        User user = new User(1234, modelUrlParser.findByName("lassie"));
+        dynamoDBMapper.save(user);
+
+        // when
+        User actual = dynamoDBMapper.load(User.class, 1234);
+
+        // then
+        assertThat(actual).isEqualTo(new User(1234, modelUrlParser.findByName("lassie")));
+    }
+
+    @Test
+    @DisplayName("User 수정")
+    void updateItem() {
+        // given
+        User user = new User(1234, modelUrlParser.findByName("lassie"));
+        dynamoDBMapper.save(user);
+        User loaded = dynamoDBMapper.load(User.class, 1234);
+
+        // when
+        User updated = loaded.changeMonitoredUrl(modelUrlParser.findByName("dexter"));
+        dynamoDBMapper.save(updated);
+
+        // then
+        User actual = dynamoDBMapper.load(User.class, 1234);
+        assertThat(actual).isEqualTo(new User(1234, modelUrlParser.findByName("dexter")));
+    }
+
+    @Test
+    @DisplayName("User 제거")
+    void deleteUser() {
+        // given
+        User user = new User(1234, modelUrlParser.findByName("lassie"));
+        dynamoDBMapper.save(user);
+
+        // when
+        dynamoDBMapper.delete(user);
+
+        // then
+        User deleted = dynamoDBMapper.load(User.class, 1234);
+        assertThat(deleted).isNull();
+    }
+
+    @AfterAll
+    void tearDown() {
+        amazonDynamoDB.deleteTable(TABLE_NAME);
+        amazonDynamoDB.shutdown();
+    }
+}
+
+
