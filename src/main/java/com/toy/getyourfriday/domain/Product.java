b6df6255c4542c194c3e648c40f2ef94fe67d7b2
Jay <beginin15@gmail.com>
2021-01-18 00:48:27 +0900

    [#20] test: Products 메소드 테스트
    - Product.java: 동등성 구현

--- src/main/java/com/toy/getyourfriday/domain/Product.java
+++ src/main/java/com/toy/getyourfriday/domain/Product.java
@@ -1,14 +1,13 @@
 package com.toy.getyourfriday.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode
 public class Product {
 
     private final String link;
-
-    public Product(String link) {
-        this.link = link;
-    }
-
-    public String getLink() {
-        return link;
-    }
 }
---
+++ src/test/java/com/toy/getyourfriday/domain/ProductsTest.java
@@ -0,0 +1,99 @@
+package com.toy.getyourfriday.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProductsTest {
+
+    private Products previous;
+
+    @BeforeEach
+    void setUp() {
+        this.previous = createProductsByLinks(
+                "https://www.freitag.ch/ko/f11?productID=1143300",
+                "https://www.freitag.ch/ko/f11?productID=1135801"
+        );
+    }
+
+    @Test
+    @DisplayName("업데이트, 판매 모두 없음")
+    void isUpdatedWhenNotUpdatedAndNotSold() {
+        Products latest = createProductsByLinks(
+                "https://www.freitag.ch/ko/f11?productID=1143300",
+                "https://www.freitag.ch/ko/f11?productID=1135801"
+        );
+        assertThat(latest.isUpdated(previous)).isFalse();
+    }
+
+    @Test
+    @DisplayName("업데이트 개수와 판매 개수가 동일")
+    void isUpdatedWhenTotalSame() {
+        Products latest = createProductsByLinks(
+                "https://www.freitag.ch/ko/f11?productID=1143300",
+                "https://www.freitag.ch/ko/f11?productID=1135822"
+        );
+        assertThat(latest.isUpdated(previous)).isTrue();
+    }
+
+    @Test
+    @DisplayName("업데이트 개수가 더 많음")
+    void isUpdatedWhenUpdatedMore() {
+        Products latest = createProductsByLinks(
+                "https://www.freitag.ch/ko/f11?productID=1143300",
+                "https://www.freitag.ch/ko/f11?productID=1135801",
+                "https://www.freitag.ch/ko/f11?productID=1135822"
+        );
+        assertThat(latest.isUpdated(previous)).isTrue();
+    }
+
+    @Test
+    @DisplayName("판매만 발생")
+    void isUpdatedWhenOnlySold() {
+        Products latest = createProductsByLinks(
+                "https://www.freitag.ch/ko/f11?productID=1143300"
+        );
+        assertThat(latest.isUpdated(previous)).isFalse();
+    }
+
+    @Test
+    @DisplayName("업데이트 개수보다 판매 개수가 더 많은 경우")
+    void isUpdatedWhenSoldMore() {
+        Products latest = createProductsByLinks(
+                "https://www.freitag.ch/ko/f11?productID=1135822"
+        );
+        assertThat(latest.isUpdated(previous)).isTrue();
+    }
+
+    @Test
+    @DisplayName("업데이트된 제품들만 분류")
+    void getUpdatedProducts() {
+        // given
+        Products latest = createProductsByLinks(
+                "https://www.freitag.ch/ko/f11?productID=1143300",
+                "https://www.freitag.ch/ko/f11?productID=1135801",
+                "https://www.freitag.ch/ko/f11?productID=1135822"
+        );
+
+        // when
+        Products updatedProducts = latest.getUpdatedProducts(previous);
+
+        // then
+        assertThat(updatedProducts).isEqualTo(createProductsByLinks(
+                "https://www.freitag.ch/ko/f11?productID=1135822")
+        );
+    }
+
+    public static Products createProductsByLinks(String...links) {
+        List<Product> products = Arrays.stream(links)
+                .map(Product::new)
+                .collect(Collectors.toList());
+        return new Products(products);
+    }
+}
