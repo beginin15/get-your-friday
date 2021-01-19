0fa4ed8eb9604db0ba41166cf779dee148f1e1a9
Jay <beginin15@gmail.com>
2021-01-20 00:00:17 +0900

    [#20] refactor: Product 롬복 toString 어노테이션 추가

--- src/main/java/com/toy/getyourfriday/domain/Product.java
+++ src/main/java/com/toy/getyourfriday/domain/Product.java
@@ -3,10 +3,12 @@ package com.toy.getyourfriday.domain;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
 @EqualsAndHashCode
+@ToString
 public class Product {
 
     private final String link;
