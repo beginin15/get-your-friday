01a7a05aac5142be57ce358de471321ff616e79c
Jay <beginin15@gmail.com>
2021-01-20 19:32:45 +0900

    [#4] test: WebDriver와 RemoteWebDriver의 인스턴스 생성 시간 비교
    - RemoteWebDriver는 테스트를 별도의 remote 환경에서 수행할 때 사용
    - 현재는 selenium 테스트를 위한 별도의 remote server가 존재하지 않기 때문에 일반적인 WebDriver와
      크게 다르지 않다고 이해

--- src/test/java/com/toy/getyourfriday/selenium/WebDriverTest.java
+++ src/test/java/com/toy/getyourfriday/selenium/WebDriverTest.java
@@ -1,19 +1,29 @@
 package com.toy.getyourfriday.selenium;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class WebDriverTest {
 
@@ -105,6 +115,24 @@ public class WebDriverTest {
         assertNotEquals(ipAddress1, ipAddress2);
     }
 
+    @Test
+    @RepeatedTest(value = 10)
+    @DisplayName("WebDriver와 RemoteWebDriver 인스턴스 생성 시간 비교")
+    void compareWithRemoteWebDriver() {
+        long before = System.currentTimeMillis();
+        WebDriver remoteWebDriver = new RemoteWebDriver(driverService.getUrl(), new ChromeOptions());
+        long instanceTimeOfRemote = System.currentTimeMillis() - before;
+
+        before = System.currentTimeMillis();
+        WebDriver driverDefault = new ChromeDriver(new ChromeOptions());
+        long instanceTimeOfDefault = System.currentTimeMillis() - before;
+
+        assertThat(instanceTimeOfDefault > instanceTimeOfRemote).isTrue();
+        System.out.println("Default: " + instanceTimeOfDefault + ", Remote: " + instanceTimeOfRemote);
+
+        closeDriver(remoteWebDriver, driverDefault);
+    }
+
     private WebDriver createDriverByOptions(Proxy proxy, boolean headless) {
         ChromeOptions options = new ChromeOptions();
         if (proxy != null) {
@@ -112,5 +140,12 @@ public class WebDriverTest {
         }
         options.setHeadless(headless);
         return new RemoteWebDriver(driverService.getUrl(), options);
+    }
+
+    private void closeDriver(WebDriver...drivers) {
+        Arrays.stream(drivers).forEach(d -> {
+            d.close();
+            d.quit();
+        });
     }
 }
