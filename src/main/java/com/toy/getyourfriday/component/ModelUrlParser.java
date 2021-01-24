0f517fb677d6588cbb198170fbe5dd5d56612920
Jay <beginin15@gmail.com>
2021-01-25 01:01:27 +0900

    [#28] feat: ModelUrlParser.getAllModelNames 메소드 구현

--- src/main/java/com/toy/getyourfriday/component/ModelUrlParser.java
+++ src/main/java/com/toy/getyourfriday/component/ModelUrlParser.java
@@ -1,11 +1,16 @@
 package com.toy.getyourfriday.component;
 
-import com.toy.getyourfriday.domain.ModelUrl;
+import com.google.common.collect.ImmutableList;
+import com.toy.getyourfriday.domain.scraping.ModelUrl;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Map;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
 
 @Component
 @PropertySource("classpath:models.properties")
@@ -16,5 +21,11 @@ public class ModelUrlParser {
 
     public ModelUrl findByName(String name) {
         return BY_NAME.get(name);
+    }
+
+    public List<String> getAllModelNames() {
+        return BY_NAME.keySet()
+                .stream()
+                .collect(collectingAndThen(toList(), ImmutableList::copyOf));
     }
 }
