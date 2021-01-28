505c891581845a0952d993c21f28154158afda1b
Jay <beginin15@gmail.com>
2021-01-29 01:51:57 +0900

    [#28] feat: 구현체에서 발생한 통신 에러를 Mono.error로 맵핑

--- src/main/java/com/toy/getyourfriday/domain/response/WebClientResponse.java
+++ src/main/java/com/toy/getyourfriday/domain/response/WebClientResponse.java
@@ -2,8 +2,13 @@ package com.toy.getyourfriday.domain.response;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriBuilder;
+import reactor.core.publisher.Mono;
 
 import java.net.URI;
 
@@ -13,7 +18,18 @@ public abstract class WebClientResponse {
 
     protected Integer chatId;
 
-    public abstract void send(WebClient webClient);
+    protected static Mono<? extends Throwable> mapToHttpStatusCodeException(ClientResponse response) {
+        HttpStatus statusCode = response.statusCode();
+        if (statusCode.is4xxClientError()) {
+            return Mono.error(new HttpClientErrorException(statusCode));
+        }
+        if (statusCode.is5xxServerError()) {
+            return Mono.error(new HttpServerErrorException(statusCode));
+        }
+        return response.createException();
+    }
+
+    public abstract Mono<?> send(WebClient webClient);
 
     protected abstract URI buildUri(UriBuilder uriBuilder);
 }
